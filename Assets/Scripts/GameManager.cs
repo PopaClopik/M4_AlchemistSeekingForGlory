e7fe7b1467770357f5bca5846e96bb5e68ad6801
PyroSanta <matveyabramov09@gmail.com>
2024-04-19 23:36:45 +0300

    FirstPrototypeMainMenu
    Первый прототип полурабочего меню

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+     public void StartGame()
+    {
+        SceneManager.LoadScene("VillageScene");
+    }
+
+    
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+}
