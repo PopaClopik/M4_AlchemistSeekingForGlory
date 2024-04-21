7578483aec71315b426210d647b09f99af55f1fe
PyroSanta <matveyabramov09@gmail.com>
2024-04-21 21:42:18 +0300

    MainMenuIsDone
    сделал игровую менюшку и паузу.

---
+++ Assets/Scripts/SoundToggleInPause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundToggleInPause : MonoBehaviour
+{
+    public GameObject _volumeMute;
+    public GameObject _volumeUnmute;
+
+
+    public void SoundToggleButton()
+    {
+        if (AudioListener.volume == 0)
+        {
+            AudioListener.volume = 1;
+            _volumeMute.SetActive(true);
+            _volumeUnmute.SetActive(false);
+        }
+
+        else
+        {
+            AudioListener.volume = 0;
+            _volumeUnmute.SetActive(true);
+            _volumeMute.SetActive(false);
+        }
+    }
+}
