dc71e1d82f4c91ce1a97dcd29d7584ab02520708
PopaClopik <komarovartem9962@gmail.com>
2024-05-04 22:33:55 +0300

    ShootingStart
    Начал работу над стрельбой персонажа, пока что это только промежуточный результат, надо еще работать

---
+++ Assets/Scripts/StonesCaster.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StonesCaster: MonoBehaviour
+{
+    public List<GameObject> Stones = new List<GameObject>();
+
+    //public Rigidbody grenadePrefab;
+    public Transform stoneSourceTransform;
+
+    public float force = 10;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var stoneToSpawn = Stones[Random.Range(0, Stones.Count)];
+
+            var spawnedStone = Instantiate(stoneToSpawn);
+            spawnedStone.transform.position = stoneSourceTransform.position;
+            spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * force);
+        }
+    }
+}
