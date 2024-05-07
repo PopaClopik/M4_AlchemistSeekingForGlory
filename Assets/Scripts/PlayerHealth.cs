ef1f81faafefd7b55c427ffcf1fe4c9f36da0e68
PopaClopik <komarovartem9962@gmail.com>
2024-05-07 23:02:59 +0300

    PlayerHealth
    сделал скрипт на здоровье игрока, проверить пока не могу, так как нет врагов, но должен работать))

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float value = 100;
+    public RectTransform valueRectTransform;
+
+    private float _maxValue;
+
+    private void Start()
+    {
+        _maxValue = value;
+        DrawHealthBar();
+    }
+
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if (value <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        DrawHealthBar();
+    }
+
+    private void DrawHealthBar()
+    {
+        valueRectTransform.anchorMax = new Vector2(value / _maxValue, 1);
+    }
+
+}
