e2419167d05ad9e3c7c7dfc1f7894ab2c9a3a2f5
PopaClopik <komarovartem9962@gmail.com>
2024-05-10 23:09:17 +0300

    MobPatrolPoints
    Накинул на первого моба скрипты, он движется к заданным патрульным точкам. Работа продолжается

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Animator animator;
+    public List<Transform> patrolPoints;
+    public PlayerController player;
+    public float viewAngle;
+    public float damage = 20;
+    public float attackDistance = 1f;
+
+    private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+    private PlayerHealth _playerHealth;
+    private EnemyHealth _enemyHealth;
+
+    public bool IsAlive()
+    {
+        return _enemyHealth.IsAlive();
+    }
+
+    private void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _playerHealth = player.GetComponent<PlayerHealth>();
+        _enemyHealth = GetComponent<EnemyHealth>();
+    }
+    private void Update()
+    {
+        NoticePlayerUpdate();
+        ChaseUpdate();
+        AttackUpdate();
+        PatrolUpdate();
+
+    }
+    private void AttackUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                //animator.SetTrigger("Attack");
+            }
+        }
+    }
+
+    public void AttackDamage()
+    {
+        if (!_isPlayerNoticed) return;
+        if (_navMeshAgent.remainingDistance > (_navMeshAgent.stoppingDistance + attackDistance)) return;
+
+        _playerHealth.DealDamage(damage);
+    }
+
+    private void NoticePlayerUpdate()
+    {
+
+        _isPlayerNoticed = false;
+        //if (!_playerHealth.IsAlive()) return;
+
+        var direction = player.transform.position - transform.position;
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+    }
+    private void PatrolUpdate()
+    {
+        if (!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                PickNewPatrolPoint();
+            }
+        }
+    }
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float value = 100;
+    public Animator animator;
+
+    //public PlayerProgress playerProgress;
+
+    private void Start()
+    {
+        //playerProgress = FindObjectOfType<PlayerProgress>();
+    }
+
+    public bool IsAlive()
+    {
+        return value > 0;
+    }
+
+    public void DealDamage(float damage)
+    {
+        //playerProgress.AddExperience(damage);
+
+        value -= damage;
+        if (value <= 0)
+        {
+            EnemyDeath();
+        }
+        else
+        {
+            animator.SetTrigger("Hit");
+        }
+    }
+
+    private void EnemyDeath()
+    {
+        //animator.SetTrigger("Death");
+        GetComponent<EnemyAI>().enabled = false;
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<CapsuleCollider>().enabled = false;
+    }
+}
