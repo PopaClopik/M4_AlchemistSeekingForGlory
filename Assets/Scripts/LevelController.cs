e4604366edbe6df859d10ee04b8a89848d0c14c5
Артём <61494745+Warycarpert12@users.noreply.github.com>
2024-05-16 13:55:23 +0300

    LevelButton
    Добавлена кнопка перехода между уровнями

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    public void LoadNextLevel()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(3);
+    }
+}
