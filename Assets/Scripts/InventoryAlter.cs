4a777f05385eb74bb6afa2ef77852b93ba7d0481
denq113 <86840750+denq113@users.noreply.github.com>
2024-05-18 15:18:45 +0700

    InventoryAlter
    Готов по сути скрипт инвенторя но нужно добавить визуал

---
+++ Assets/Scripts/InventoryAlter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryAlter : MonoBehaviour
+{
+    public int gipnoGrib;
+    public int muhomorApetitGrib;
+    public int pogankaObikGrib;
+    public int fireGrib;
+    public int strongestGrib;
+
+    public int polan;
+    public int boyaresnic;
+    public int borhevic;
+
+    public int strongestPotion;
+    public int hillPotion;
+    public int unvisibalPotion;
+    public int unbornPotion;
+    public int slipPotion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ItemCount() 
+    {
+
+    }
+
+}
