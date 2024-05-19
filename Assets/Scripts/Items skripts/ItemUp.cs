9ad220fe183ca15c171eafadfd1ef8c81a5780fe
denq113 <86840750+denq113@users.noreply.github.com>
2024-05-20 02:12:05 +0700

    ItemUp
    Создал скрипт подбора предметов

---
+++ Assets/Scripts/Items skripts/ItemUp.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemUp : MonoBehaviour
+{
+    public InventoryAlter InventoryAlter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Дотронулся");
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            InventoryAlter.fireGrib += 1;
+            Debug.Log("+1 гриб");
+            Destroy(gameObject);
+        }
+    }
+   
+    //private void 
+    //{
+    //  Debug.Log("Дотронулся");
+    //if (Input.GetKeyDown(KeyCode.E))
+    //{
+    //  InventoryAlter.fireGrib += 1;
+    //Debug.Log("+1 гриб");
+    //}
+    //}
+}
