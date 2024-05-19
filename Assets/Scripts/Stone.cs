23a2a9ff1eb68fbec75912f58ebd1c1ea6fc0e04
PopaClopik <komarovartem9962@gmail.com>
2024-05-19 22:06:22 +0300

    DamageMobs
    теперь можно дамажить мобов

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -6,10 +6,27 @@ public class Stone : MonoBehaviour
 {
     public float damage = 20;
     public float lifeTime;
-   
+
     void Start()
     {
         Invoke("DestroyStone", lifeTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyStone();
+    }
+
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+
+        DestroyStone();
     }
 
     private void DestroyStone()
