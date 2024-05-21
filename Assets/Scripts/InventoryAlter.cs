765413d8366aaa333b72d7bfd10c6c4ad663a287
denq113 <86840750+denq113@users.noreply.github.com>
2024-05-21 19:45:46 +0700

    InventiryFinal
    Добавил грибам и цветам физику сделал выкидование их на 1 - 8 клавишы и теперь если их нет в инвенторе то они не появятся

--- Assets/Scripts/InventoryAlter.cs
+++ Assets/Scripts/InventoryAlter.cs
@@ -25,6 +25,13 @@ public class InventoryAlter : MonoBehaviour
 
     public GameObject Player;
     public GameObject gipnoGribObgect;
+    public GameObject muhomorApetitGribObgect;
+    public GameObject pogankaObikGribObject;
+    public GameObject fireGribObject;
+    public GameObject strongestGribObject;
+    public GameObject polanObject;
+    public GameObject boyaresnicObject;
+    public GameObject borhevicObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -44,15 +51,128 @@ public class InventoryAlter : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            gipnoGribMetod();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            muhomorApetitGribMetod();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            pogankaObikGribMetod();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            fireGribMetod();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            strongestGribMetod();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            polanMetod();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            boyaresnicMetod();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            borhevicMetod();
+        }
+
+    }
+
+    private void gipnoGribMetod()
+    {
+        if (gipnoGrib > 0)
+        {
             Debug.Log("Сработало");
             var spawnedStone = Instantiate(gipnoGribObgect);
             spawnedStone.transform.position = stoneSourceTransform.position;
             spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * 0);
+            gipnoGrib -= 1; 
         }
     }
-    public void ItemCount() 
+    private void muhomorApetitGribMetod()
     {
-
+        if (muhomorApetitGrib>0)
+        {
+            Debug.Log("Сработало");
+            var spawnedStone = Instantiate(muhomorApetitGribObgect);
+            spawnedStone.transform.position = stoneSourceTransform.position;
+            spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * 0);
+            muhomorApetitGrib -= 1;
+        }
     }
+    private void pogankaObikGribMetod()
+    {
+        if (pogankaObikGrib > 0)
+        {
+            Debug.Log("Сработало");
+            var spawnedStone = Instantiate(pogankaObikGribObject);
+            spawnedStone.transform.position = stoneSourceTransform.position;
+            spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * 0);
+            pogankaObikGrib -= 1;
+        }
+    }
+    private void fireGribMetod()
+    {
+        if (fireGrib > 0)
+        {
+            Debug.Log("Сработало");
+            var spawnedStone = Instantiate(fireGribObject);
+            spawnedStone.transform.position = stoneSourceTransform.position;
+            spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * 0);
+            fireGrib -= 1;
+        }
+    }
+    private void strongestGribMetod()
+    {
+        if (strongestGrib > 0)
+        {
+            Debug.Log("Сработало");
+            var spawnedStone = Instantiate(strongestGribObject);
+            spawnedStone.transform.position = stoneSourceTransform.position;
+            spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * 0);
+            strongestGrib -= 1;
+        }
+    }
+    private void polanMetod()
+    {
+        if (polan > 0)
+        {
+            Debug.Log("Сработало");
+            var spawnedStone = Instantiate(polanObject);
+            spawnedStone.transform.position = stoneSourceTransform.position;
+            spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * 0);
+            polan -= 1;
+        }
+    }
+    private void boyaresnicMetod()
+    {
+        if (boyaresnic > 0)
+        {
+            Debug.Log("Сработало");
+            var spawnedStone = Instantiate(boyaresnicObject);
+            spawnedStone.transform.position = stoneSourceTransform.position;
+            spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * 0);
+            boyaresnic -= 1;
+        }
+    }
+    private void borhevicMetod()
+    {
+        if (borhevic > 0)
+        {
+            Debug.Log("Сработало");
+            var spawnedStone = Instantiate(borhevicObject);
+            spawnedStone.transform.position = stoneSourceTransform.position;
+            spawnedStone.GetComponent<Rigidbody>().AddForce(stoneSourceTransform.forward * 0);
+            borhevic -= 1;
+        }
+    }
+
+   
 
 }
