17c9f29b8af6fbff5ac4903ea2fe53eae4daee2b
Артём <61494745+Warycarpert12@users.noreply.github.com>
2024-05-22 19:57:37 +0300

    QuestSpesnerAdd
    Начата работа над квестами

---
+++ Assets/Scripts/Quests/QuestSpenser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestSpenser : MonoBehaviour
+{
+    public bool EndDialog;
+    public GameObject Dialog1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (EndDialog == true)
+        {
+            Time.timeScale = 1;
+            Dialog1.SetActive(false);
+        }
+
+        
+    }
+    void OnTriggerEnter(Collider col)
+        {
+            if (col.gameObject.tag == "Player");
+            Time.timeScale = 0;
+            Dialog1.SetActive(true);
+        }
+}
