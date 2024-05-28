d3631a4a77b1ce17d86d422afb1571f40ab2a6fb
PyroSanta <matveyabramov09@gmail.com>
2024-05-28 23:28:21 +0300

    MadeRicepeBook
    Создал книгу рецептов

---
+++ Assets/Scripts/InteractiveButton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveButton : MonoBehaviour
+{
+    public GameObject targetCanvas;
+    private bool isPlayerInRange = false;
+    public GameObject _cookingClue;
+
+    void Start()
+    {
+        if (targetCanvas != null)
+        {
+            targetCanvas.SetActive(false); // Начально Canvas выключен
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            if (targetCanvas != null)
+            {
+                _cookingClue.SetActive(false);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                targetCanvas.SetActive(true); // Включаем Canvas при нажатии клавиши E
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _cookingClue.SetActive(true);
+            isPlayerInRange = true;
+            // Вы можете добавить визуальные подсказки, например, отобразить текст "Нажмите E для взаимодействия"
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _cookingClue.SetActive(false);
+            isPlayerInRange = false;
+            // Скрыть визуальные подсказки
+        }
+    }
+}
+
---
+++ Assets/Scripts/ReceptCanvasController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceptCanvasController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+   public void CursorLock()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
